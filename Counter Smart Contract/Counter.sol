b337488a6ade66d305d54b7bebf3742cbdb238a9
DebjitPramanick <dpramanick9990@gmail.com>
2021-05-16 09:35:28 +0530

    Counter Smart Contract

---
+++ Counter Smart Contract/Counter.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.8.1;
+
+contract Counter{
+
+    // Declaring count variable
+    uint count = 0;
+    
+    // Declaring events
+    event Increment(uint value);
+    event Decrement(uint value);
+
+
+    // Declaring getCount function to get the current value of count
+    function getCount() view public returns(uint) {
+        return count;
+    }
+    
+    // Declaring increment function to increase the current value of count by 1
+    function increment() public {
+        count = count + 1;
+        emit Increment(count);
+    }
+
+    // Declaring decrement function to decrease the current value of count by 1
+    function decrement() public {
+        count = count - 1;
+        emit Increment(count);
+    }
+}
